d86625374c78ff751500c544bc3cec1d1f8ec2ad
yurii-bezus <bylchenok@gmail.com>
2025-06-01 09:53:20 +0000

    Deploying to gh-pages from @ yurii-bezus/online_store@d609aababd7b4f06571558c725fc2fab529e1771 🚀

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import"./assets/styles-BK7AYJoX.js";import{a as o}from"./assets/vendor-N5iQpiFS.js";o.defaults.baseURL="https://dummyjson.com";async function r(){const t=await o.get("/products/category-list");return console.log(t),t.data}async function c(){return(await o.get("https://dummyjson.com/products?limit=12&skip=0")).data.products}const a={allCategories:document.querySelector(".categories"),allProducts:document.querySelector(".products")};function n(t){a.allCategories.innerHTML="";const e=t.map(s=>`<li class="categories__item">
+import"./assets/styles-BK7AYJoX.js";import{a}from"./assets/vendor-N5iQpiFS.js";a.defaults.baseURL="https://dummyjson.com";async function i(){const t=await a.get("/products/category-list");return console.log(t),t.data}async function n(){return(await a.get("https://dummyjson.com/products?limit=12&skip=0")).data.products}async function l(t){const e=await a.get(`products/category/${t}`);return console.log(e),e.data.products}const o={allCategories:document.querySelector(".categories"),allProducts:document.querySelector(".products"),notFound:document.querySelector(".not-found")};function u(t){o.allCategories.innerHTML="";const e=t.map(s=>`<li class="categories__item">
    <button class="categories__btn" type="button">${s}</button>
- </li>`).join("");a.allCategories.innerHTML=e}function i(t){a.allProducts.innerHTML="";const e=t.map(s=>`<li class="products__item" data-id="${t.id}">
+ </li>`).join("");o.allCategories.innerHTML=e}function c(t){if(t.length===0){o.notFound.classList.add("not-found--visible");return}o.notFound.classList.remove("not-found--visible"),o.allProducts.innerHTML="";const e=t.map(s=>`<li class="products__item" data-id="${t.id}">
     <img class="products__image" src="${s.images[0]}" alt="${s.title}"/>
     <p class="products__title">${s.title}</p>
     <p class="products__brand"><span class="products__brand--bold">Brand:${s.brand}</span></p>
     <p class="products__category">Category:${s.category} </p>
     <p class="products__price">Price:${s.price} $</p>
  </li>
-`).join("");a.allProducts.innerHTML=e}async function l(){const t=await r();n(t)}l();async function u(){const t=await c();i(t)}u();a.allCategories.addEventListener("click",t=>{const e=t.target.closest(".categories__btn");console.log(e.textContent)});
+`).join("");o.allProducts.innerHTML=e}async function d(){const e=["All",...await i()];u(e)}d();async function p(){const t=await n();c(t)}p();o.allCategories.addEventListener("click",async t=>{const e=t.target.closest(".categories__btn");if(!e)return;const s=e.textContent.trim();try{let r=[];s==="All"?r=await n():r=await l(s),c(r)}catch(r){console.error("Error loading products:",r)}});
 //# sourceMappingURL=index.js.map
